c8331be4175cdc7036bc174f7c953acbe08af2b7
prashant jyanth <prashant.jyanth@gmail.com>
2025-07-22 18:43:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    while True:
+        print("Running main loop...")
+        time.sleep(1)
+        print("Still running...")
+
+if __name__ == "__main__":
+    main()
